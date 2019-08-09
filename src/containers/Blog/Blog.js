ada71b0ceb22e17ee02cb1f4f994d45c7be8b5b0
Tony <iamtonycheung@gmail.com>
2019-08-09 16:54:23 +0800

    after lesson 242

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,18 +1,25 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 //import FullPost from './FullPost/FullPost';
 import Posts from './Posts/Posts';
-
-import NewPost from './NewPost/NewPost';
+//import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
 import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
-
 import "./Blog.css";
 //import axios from '../../axios';
 
-class Blog extends Component {
-   render() {
-   
+ const AsyncNewPost=asyncComponent(()=>{
+    return import('./NewPost/NewPost');
+});
 
+class Blog extends Component {
+  state ={ 
+    auth: true
+  };
+
+ 
+
+   render() {
     return (
       <div className="Blog">
         <header>
@@ -35,10 +42,11 @@ class Blog extends Component {
             </nav>
         </header>
         <Switch>
-          <Route path="/new-post"  component={NewPost} />
+          {this.state.auth? <Route path="/new-post"  component={AsyncNewPost} />:null}
           <Route path="/posts" component={Posts} />
-          <Redirect from="/" to="/posts"  />
-        {/* <Route path="/" component={Posts} /> */}
+          <Route render={()=><h1>Not Found</h1>}/>
+         {/* <Redirect from="/" to="/posts"  /> */}
+         {/* <Route path="/" component={Posts} /> */}
         </Switch>
       </div>
     );
--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -13,6 +13,7 @@ class NewPost extends Component {
     }
 
     componentDidMount(){
+        //this.props.history.replace('/posts');
         console.log(this.props);
     }
 
